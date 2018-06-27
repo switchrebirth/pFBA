c6a3cd25d5debdce7d492087f1f13b068de326ad
cpasjuste <cpasjuste@gmail.com>
2018-06-27 09:19:03 +0200

    fix new inputs

--- pfba/uiEmu.cpp
+++ pfba/uiEmu.cpp
@@ -58,9 +58,9 @@ int PFBAGuiEmu::run(C2DUIRomList::Rom *rom) {
     printf("Initialize driver...\n");
     if (DrvInit(rom->drv, false) != 0) {
         printf("\nDriver initialisation failed! Likely causes are:\n"
-               "- Corrupt/Missing ROM(s)\n"
-               "- I/O Error\n"
-               "- Memory error\n\n");
+                       "- Corrupt/Missing ROM(s)\n"
+                       "- I/O Error\n"
+                       "- Memory error\n\n");
         DrvExit();
         InpExit();
         getUi()->getUiProgressBox()->setVisibility(Hidden);
@@ -184,9 +184,7 @@ void PFBAGuiEmu::updateFrame() {
 
 int PFBAGuiEmu::update() {
 
-    inputServiceSwitch = 0;
-    inputP1P2Switch = 0;
-
+    bool combo = false;
     int rotation_config =
             getUi()->getConfig()->getValue(C2DUIOption::Index::ROM_ROTATION, true);
     int rotate_input = 0;
@@ -221,6 +219,9 @@ int PFBAGuiEmu::update() {
     }
 #endif
 
+    inputServiceSwitch = 0;
+    inputP1P2Switch = 0;
+
     Input::Player *players = getUi()->getInput()->update(rotate_input);
 
     // look for player 1 menu combo
@@ -234,24 +235,27 @@ int PFBAGuiEmu::update() {
 
     // look each players for combos keys
     for (int i = 0; i < PLAYER_COUNT; i++) {
-
-        // allow devices with single select/start button to send start/coins (switch in single joycon mode)
+        // allow devices with single select/start button to send start/coins (nsw in single joycon mode)
         if (((players[i].state & Input::Key::KEY_START) && (players[i].state & Input::Key::KEY_FIRE1))
             || ((players[i].state & Input::Key::KEY_COIN) && (players[i].state & Input::Key::KEY_FIRE1))) {
             players[i].state = Input::Key::KEY_START;
+            combo = true;
         } else if (((players[i].state & Input::Key::KEY_START) && (players[i].state & Input::Key::KEY_FIRE2))
                    || ((players[i].state & Input::Key::KEY_COIN) && (players[i].state & Input::Key::KEY_FIRE2))) {
             players[i].state = Input::Key::KEY_COIN;
+            combo = true;
         }
     }
 
     // look for player 1 combos key
-    if (((players[0].state & Input::Key::KEY_START) && (players[0].state & Input::Key::KEY_FIRE3))
-        || ((players[0].state & Input::Key::KEY_COIN) && (players[0].state & Input::Key::KEY_FIRE3))) {
-        inputServiceSwitch = 1;
-    } else if (((players[0].state & Input::Key::KEY_START) && (players[0].state & Input::Key::KEY_FIRE4))
-               || ((players[0].state & Input::Key::KEY_COIN) && (players[0].state & Input::Key::KEY_FIRE4))) {
-        inputP1P2Switch = 1;
+    if (!combo) {
+        if (((players[0].state & Input::Key::KEY_START) && (players[0].state & Input::Key::KEY_FIRE3))
+            || ((players[0].state & Input::Key::KEY_COIN) && (players[0].state & Input::Key::KEY_FIRE3))) {
+            inputServiceSwitch = 1;
+        } else if (((players[0].state & Input::Key::KEY_START) && (players[0].state & Input::Key::KEY_FIRE4))
+                   || ((players[0].state & Input::Key::KEY_COIN) && (players[0].state & Input::Key::KEY_FIRE4))) {
+            inputP1P2Switch = 1;
+        }
     }
 
     // look for window resize event
